99efc34ced88a190a3336a313fe15ea802b53c26
OWU131323203 <s1323203@cst.otsuma.ac.jp>
2025-05-28 12:05:33 +0900

    Create test_0528.py

---
+++ test_0528.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("第7回 Streamlit フォーム演習 - テンプレート")
+st.caption("st.form を使ってサークル入会申し込みフォームを作成しましょう。")
+
+st.markdown("---")
+st.subheader("演習: サークル入会申し込みフォーム")
+st.write("**課題**: フォームを使って、サークル入会の申し込み情報をまとめて処理するアプリを作成する。")
